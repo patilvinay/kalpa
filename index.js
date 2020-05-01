95719e1461a8198e42922887a6f2577f2704b683
Vinay <vinay.hpatil@gmail.com>
2020-05-01 17:39:53 +0000

    feat(task): Added task.yml, this file is used as input
    tasks.yml is used as input file. Jobs in the file are executed one by
    one and code is generated

--- index.js
+++ index.js
@@ -1,28 +1,43 @@
-
 YAML = require('yamljs');
-const fs = require('fs');
+const fs = require('./lib/fs');
+const _fs = require('fs');
 let ejs = require('ejs');
+const path = require('path')
 
-nativeObject = YAML.load('entity.yaml');
+let projects = YAML.load('tasks.yml').project;
 
-let job = YAML.load('schema.yaml').job;
-console.log("Total jobs %s",job.length)
-for(i=0;i<job.length;i++){
-    //nsole.log("looping %d",i)
-     nativeObject = YAML.load(job[i].input);
-     //console.dir(nativeObject, {depth:8, colors: true})
-     var str = fs.readFileSync(job[i].template,{encoding:'utf8', flag:'r'}); 
-     str= ejs.render(str, {entity:nativeObject.entity}, null);
-     console.log(str)
-     fs.writeFileSync(job[i].output, str);
-    
-}
+for (let index = 0; index < projects.length; index++) {
 
 
-// console.dir(nativeObject, {depth:8, colors: true})
-// var str = fs.readFileSync('model-template.ejs.js',{encoding:'utf8', flag:'r'}); 
-// str= ejs.render(str, {entity:nativeObject.entity}, null);
-// console.log(str)
-// fs.writeFileSync('model.js', str);
+    //******************************Creates Project directory ******************************************** */ 
+    let prj = projects[index]
+    fs.mkDirByPathSync(prj.name)
+    let jobs = prj.jobs;
+    console.log("Total jobs %s", jobs.length)
+    for (i = 0; i < jobs.length; i++) {
+
+        let job = jobs[i]
+        nativeObject = YAML.load(job.input);
+
+        var str = _fs.readFileSync(job.template, {
+                encoding: 'utf8',
+                flag: 'r'});
+
+            str = ejs.render(str, {
+                entity: nativeObject.entity
+            }, null);
+
+            console.log(str);
+            writePath = path.join(prj.name, prj.jobs[i].dir, prj.jobs[i].output);
+            console.log("writing in %s", writePath);
+            fs.writeFileSync(writePath, str);
+
+        }
+    }
 
 
+    // console.dir(nativeObject, {depth:8, colors: true})
+    // var str = fs.readFileSync('model-template.ejs.js',{encoding:'utf8', flag:'r'}); 
+    // str= ejs.render(str, {entity:nativeObject.entity}, null);
+    // console.log(str)
+    // fs.writeFileSync('model.js', str);
