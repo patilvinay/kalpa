bc476151cdaeac8e43eb4463bee242a8d5ee6762
Vinay <vinay.hpatil@gmail.com>
2020-05-02 11:27:20 +0000

    refactor(test): refactor

--- test.js
+++ test.js
@@ -3,12 +3,13 @@ const fs = require('./lib/fs');
 const _fs = require('fs');
 let ejs = require('ejs');
 const path = require('path')
+const kfile=require("kalpa-file")
 const scaffold = require("./lib/scaffold/scaffold.js")
 yaml= require('./lib/yaml/yaml')
 const chalk = require('chalk');
 
 //let playbook = YAML.load('examples/file.yml');
-let ret =yaml.load('examples/file1.yml')
+let ret =yaml.load('examples/file.yml')
 console.log(ret.code)
 let playbook=ret.obj
 
@@ -21,19 +22,7 @@ for (let index = 0; index < playbook.length; index++) {
     let chapter = playbook[index]
     let jobs = chapter.jobs;
     console.log("Playing chapter %s", chapter.name)
-    
-    // for (i = 0; i < jobs.length; i++) {
-    //    let job = jobs[i]
-    //    console.log(chalk.blue("**************-- Executing Job:["+ job.name +"] --**************"));
-    //    var ret=scaffold.process(job)
-    //    console.log (chalk.red(ret))
-    // }
+   
 
-
-    // console.dir(nativeObject, {depth:8, colors: true})
-    // var str = fs.readFileSync('model-template.ejs.js',{encoding:'utf8', flag:'r'}); 
-    // str= ejs.render(str, {entity:nativeObject.entity}, null);
-    // console.log(str)
-    // fs.writeFileSync('model.js', str);
 }
 }
