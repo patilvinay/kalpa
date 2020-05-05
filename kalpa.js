512a48ff6f3562e619bfec7d7a5fab0935593894
Vinay <vinay.hpatil@gmail.com>
2020-05-05 19:21:55 +0000

    processing kalpa play block and series block

--- kalpa.js
+++ kalpa.js
@@ -1,5 +1,8 @@
 #!/usr/bin/env node 
 const commander = require('commander'); // include commander in git clone of commander repo
+const yaml= require("yamljs");
+const path = require ('path')
+const kalpa = require("kalpa")
 const program = new commander.Command();
 function commaSeparatedList(value, dummyPrevious) {
     return value.split(',');
@@ -11,9 +14,19 @@ program
   .action(function (cmd, env) {
     cmdValue = cmd;
     envValue = env;
-    console.log(cmd)
-    console.log(program.set);
-
+    //console.log(cmd)
+    //nsole.log(program.set);
+    let playbook={}
+    let globalData={}
+    playbook.__data={};
+    globalData.playbookDir=path.resolve('.')
+    globalData.playbookFile= cmd
+    globalData.playbookFileWitAbsolutePath = path.join(globalData.playbookDir, cmd); 
+    //nsole.log(playbook.__data.playbookFileWitAbsolutePath)
+    playbook.rootData = yaml.load(globalData.playbookFileWitAbsolutePath);
     
+    //console.log(playbook.__data.playbookdir)
+    kalpa.process(playbook,globalData);
+
   })
   .parse(process.argv);
