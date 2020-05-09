8a923d24f201e8c38f4b22942f7a78e411f18f40
Vinay <vinay.hpatil@gmail.com>
2020-05-09 17:46:29 +0000

    style(Prettier): Ran prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,24 @@
 module.exports = {
-  "env": {
-      "es6": true,
-      "node": true
+  env: {
+    es6: true,
+    node: true,
   },
-  "extends": ["airbnb-base", "plugin:prettier/recommended"],
-  "parserOptions": {
-      "ecmaVersion": 2018,
-      "sourceType": "module"
-  }
+  extends: ["airbnb-base", "plugin:prettier/recommended"],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module",
+  },
+  rules: {
+    // "no-console": "warn",
+    // "promise/always-return": "warn",
+    // "promise/no-return-wrap": "warn",
+    // "promise/param-names": "warn",
+    // "promise/catch-or-return": "warn",
+    // "promise/no-native": "off",
+    // "promise/no-nesting": "warn",
+    // "promise/no-promise-in-callback": "warn",
+    // "promise/no-callback-in-promise": "warn",
+    // "promise/no-return-in-finally": "warn",
+    // "prefer-arrow-callback": "warn",
+  },
 };
