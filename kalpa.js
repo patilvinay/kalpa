7ba61b67c50f07d471c2f6ca10e8348a6c207a94
Vinay <vinay.hpatil@gmail.com>
2020-05-09 14:30:01 +0000

    chore(linted): Linted these files

--- kalpa.js
+++ kalpa.js
@@ -1,36 +1,39 @@
 #!/usr/bin/env node
-const commander = require('commander');
-const yaml= require("yamljs");
-const path = require ('path')
-const kalpa = require("kalpa")
+const commander = require("commander");
+const yaml = require("yamljs");
+const path = require("path");
+const kalpa = require("./lib/kalpa");
+
 const program = new commander.Command();
-function commaSeparatedList(value, dummyPrevious) {
-    return value.split(',');
-  }
+function commaSeparatedList(value) {
+  return value.split(",");
+}
 
 program
-  .arguments('<filename>')
-  .option('-s, --set <type>', 'Set the variable value here, this has highest priority over everything',  commaSeparatedList)
+  .arguments("<filename>")
+  .option(
+    "-s, --set <type>",
+    "Set the variable value here, this has highest priority over everything",
+    commaSeparatedList
+  )
   .action(function (cmd, env) {
-    cmdValue = cmd;
-    envValue = env;
-    let playbook={}
-    let globalData={}
-    playbook.__data={};
-    globalData.playbookDir=path.resolve('.')
-    globalData.playbookFile= cmd
-    try{
-      globalData.playbookFileWitAbsolutePath = path.join(globalData.playbookDir, cmd);
+    const playbook = {};
+    const globalData = {};
+    playbook.__data = {};
+    globalData.playbookDir = path.resolve(".");
+    globalData.playbookFile = cmd;
+    try {
+      globalData.playbookFileWitAbsolutePath = path.join(
+        globalData.playbookDir,
+        cmd
+      );
       playbook.rootData = yaml.load(globalData.playbookFileWitAbsolutePath);
-    }catch(err){
-      kalpa.logger.error("Error loading file %s",globalData.playbookFile)
-      kalpa.logger.error(err.code)
+    } catch (err) {
+      kalpa.logger.error("Error loading file %s", globalData.playbookFile);
+      kalpa.logger.error(err.code);
       return 1;
-
     }
-    //nsole.log(kalpa)
 
-    kalpa.process(playbook,globalData);
-
+   kalpa.process(playbook, globalData);
   })
   .parse(process.argv);
--- test/test.js
+++ test/test.js
@@ -1,18 +1,21 @@
-"use strict"
-const test=require("kalpa-tester")
-const yaml=require("yamljs")
-const path = require ("path")
-const kalpa=require("kalpa")
-let obj=yaml.load("test.yaml").testing;
-//console.log(testData)
+/* eslint-disable guard-for-in */
+/* eslint-disable import/no-unresolved */
+/* eslint-disable import/newline-after-import */
+/* eslint strict: ["error", "global"] */
 
-for(let i in obj)
-{
-  let testData=obj[i]
-  //console.log(testData)
-  let result=test.test(testData.param,testData.expect);
-  if(result)
-    console.log("test passed")
-  else
-    console.log("test failed")
+const test = require("kalpa-tester");
+const yaml = require("yamljs");
+const obj = yaml.load("test.yaml").testing;
+// console.log(testData)
+
+// eslint-disable-next-line guard-for-in
+// eslint-disable-next-line no-restricted-syntax
+for (const i in obj) {
+  const testData = obj[i];
+  let result = false;
+  if (Object.prototype.hasOwnProperty.call(testData, "param")) {
+    result = test.test(testData.param, testData.expect);
+    if (result) console.log("test passed");
+    else console.log("test failed");
+  }
 }
