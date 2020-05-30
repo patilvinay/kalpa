bcf852c7e619db5f16d006e1c00fd788cd2a4ddc
vivek <vivek@ngxtechnologies.com>
2020-05-30 19:05:24 +0530

    feat(engine): Result of each chapter avalable in results(global)
    More debug examples added

--- examples/api-gen/templates/SequelizeModel.ejs.js
+++ examples/api-gen/templates/SequelizeModel.ejs.js
@@ -1,6 +1,6 @@
  // This code is generated using kalpa scafolding tool
  const Sequelize = require('sequelize');
-<% entity=entity; const items = Object.keys(entity.properties); %>
+<% entity=imported.entity; const items = Object.keys(entity.properties); %>
    const <%=entity.Name%>Schema = {
        <%for (let i=0;i<items.length;i++) {%>
         <%=items[i]%>: Sequelize.<%=entity.properties[items[i]].type%> <%if (i < items.length-1) {%>, <% } %> <% } %>
